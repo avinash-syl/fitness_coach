6522e1302bb521e388569f6f00b17c1934ab0511
Avinash S <sylendra@tcd.ie>
2023-03-22 15:12:48 +0000

    bootstrap django config

--- fitness_coach/settings.py
+++ fitness_coach/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'fit_coach',
+    'bootstrap5'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,7 +77,7 @@ WSGI_APPLICATION = 'fitness_coach.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'djongo',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
