3081bb92f867cfbe713922fb1b375ce384a2e2f3
Avinash S <sylendra@tcd.ie>
2023-04-13 14:03:43 +0100

    Updated models for user and workout plan

--- fit_coach/models.py
+++ fit_coach/models.py
@@ -1,11 +1,23 @@
 from django.db import models
+import uuid
 
-class User(models.Model):
-    username = models.CharField(max_length=50)
-    difficulty_level = models.IntegerField()
+class FitUser(models.Model):
+    user_id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=6)
+    interest = models.TextField()
+    goal = models.TextField()
+    fitness_goal = models.CharField(max_length=50)
+    def __init__(self, username, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.user_id = uuid.uuid4()
+        self.username = username
     
-    
-class Exercise(models.Model):
-    name = models.CharField(max_length=40)
+class WorkoutPlan(models.Model):
+    plan_id = models.IntegerField()
+    plan_name = models.TextField()
     description = models.TextField()
-    target_muscles = models.TextField()
+    difficulty_level = models.TextField()
+    goal = models.TextField()
+    equipment = models.TextField()
+    training_part = models.TextField()
